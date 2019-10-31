37b4b4b26202b8564c540f75058f579f1d3a248b
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-10-31 15:19:17 -0400

    Implemented Random Set Generation
    Implemented random set generation for cord pairs.

---
+++ SVM/randomSet.py
@@ -0,0 +1,21 @@
+import numpy.random
+
+def combineSets(a,b):
+    return numpy.concatenate((a,b), axis=0)
+
+def random2DGauss(mean, covMatrix):
+    numpy.random.seed(seed=4789)
+    return numpy.random.multivariate_normal(mean, covMatrix, 200)
+
+def myPoints():
+    mean1 = (-1, -1)
+    mean2 = (1, 1)
+    covarianceMatrix = [
+        [1,0],
+        [0,1]
+    ]
+    randomSet1 = random2DGauss(mean1, covarianceMatrix)
+    randomSet2 = random2DGauss(mean2, covarianceMatrix)
+    fullSet = combineSets(randomSet1, randomSet2)
+    X, Y = fullSet.T
+    return randomSet1, randomSet2, X, Y
