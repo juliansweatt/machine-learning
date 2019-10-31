e4c404c504afd7da446fbbf553a917393738109d
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-10-31 19:50:09 -0400

    Generalized Implementation for Higher n Dimensions

--- SVM/svm.py
+++ SVM/svm.py
@@ -12,28 +12,41 @@ def plotPoints(set1, set2, w, b):
     plt.axis('equal')
     print(w," |||| ", b)
 
-def plotHyperplane(w,b,minX,maxX):
+def plotHyperplane(w,b,X,Y):
     # Hyperplane 
-    x_points = numpy.array(range(minX, maxX))
+    x_points = numpy.linspace(int(min(X)), int(max(X)), len(X))
     y_points =[]
     for x in x_points:
-        y_points.append((-1 * b - w[0]*x)/w[1])
+        wTop = w[:(len(w)-1)]
+        wTop = wTop[0]
+        wBottom = w[len(w)-1]
+        y_points.append((-1 * b - sum(wTop*x))/wBottom)
+    plt.plot(x_points,y_points, color='purple')
 
     # Soft Margin
-    x_soft_upper = []
-    y_soft_upper = []
-    x_soft_lower = []
-    y_soft_lower = []
-    sm = 1/w
-    for i,x in enumerate(x_points):
-        x_soft_upper.append(x + sm[0])
-        y_soft_upper.append(y_points[i] + sm[0])
-        x_soft_lower.append(x - sm[0])
-        y_soft_lower.append(y_points[i] - sm[0])
+    for wVal in w:
+        x_soft_upper = []
+        y_soft_upper = []
+        x_soft_lower = []
+        y_soft_lower = []
+        softMargin = (1/wVal)
+        for i,x in enumerate(x_points):
+            x_soft_upper.append(x + softMargin)
+            y_soft_upper.append(y_points[i] + softMargin)
+            x_soft_lower.append(x - softMargin)
+            y_soft_lower.append(y_points[i] - softMargin)
+        plt.plot(x_soft_upper,y_soft_upper, color='grey')
+        plt.plot(x_soft_lower,y_soft_lower, color='grey')
 
-    plt.plot(x_soft_upper,y_soft_upper, color='grey')
-    plt.plot(x_soft_lower,y_soft_lower, color='grey')
-    plt.plot(x_points,y_points, color='purple')
+def plotSupportVectors(X, Y, constraints):
+    supportVectors = []
+    for i,constraint in enumerate(constraints):
+        for dual in constraint.dual_value:
+            if math.isclose(dual,0, abs_tol=0.1): # TODO Fix support vector tolerance
+                supportVectors.append((X[i],Y[i]))
+    x_sv, y_sv = numpy.asarray(supportVectors).T
+    plt.plot(x_sv, y_sv, '+', color='cyan')
+    return len(supportVectors)
 
 def optimization(X, Y, wDim):
     # Setup SVM Optimization Problem
@@ -41,10 +54,10 @@ def optimization(X, Y, wDim):
     b = cp.Variable()
     epsi = cp.Variable((len(X),1))
     half = cp.Constant(1/2)
-    reg = cp.square(cp.norm(w,2)) # Should square
+    reg = cp.square(cp.norm(w,2))
     c = cp.Constant(1)
     loss = cp.sum(cp.pos(epsi))
-    
+
     # Setup Optimization Constraints
     constraints = []
     for i in range(len(X)):
@@ -55,11 +68,12 @@ def optimization(X, Y, wDim):
     prob.solve()
 
     # Return Optimal w and b Values
-    return {'w': w.value, 'b': b.value}
+    return {'w': w.value, 'b': b.value, 'constraints': constraints}
 
 if __name__ == '__main__':
     randomSet1, randomSet2, X, Y = util.myPoints()
     optimals = optimization(X,Y, 2)
     plotPoints(randomSet1, randomSet2, optimals['w'], optimals['b'])
-    plotHyperplane(optimals['w'], optimals['b'], int(min(X)), int(max(X)))
+    plotHyperplane(optimals['w'], optimals['b'], X, Y)
+    plotSupportVectors(X,Y,optimals['constraints'])
     plt.show()
